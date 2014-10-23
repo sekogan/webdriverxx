488eaf5801ed288d0f5f113a125d53c0f923692e
Sergey Kogan <sekogan@gmail.com>
2014-10-23 21:21:25 +0400

    Add missing headers to webdriverxx.h

--- include/webdriverxx.h
+++ include/webdriverxx.h
@@ -2,7 +2,9 @@
 #define WEBDRIVERXX_H
 
 #include "webdriverxx/webdriver.h"
+#include "webdriverxx/browsers/chrome.h"
 #include "webdriverxx/browsers/firefox.h"
+#include "webdriverxx/browsers/ie.h"
 #include "webdriverxx/wait.h"
 #include "webdriverxx/wait_match.h"
 
